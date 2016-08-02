b0b8da6287249368749ef33c958ff1b8d58c1fd7
XPCegeka <ronald.dehuysser@cegeka.be>
2016-08-02 15:33:39 +0200

    [Ronald] Spacebook

--- 01 - Demo 1 - Delayed/Delayed/Delayed/Train.cs
+++ 01 - Demo 1 - Delayed/Delayed/Delayed/Train.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Delayed
 {
     public class Train
     {
+        private readonly double _hoursDelayed;
+        private Dictionary<double, string> _messageOnHoursDelayed; 
 
-        public int HoursDelayed { get; private set; }
-
-        public Train(int hoursDelayed)
+        public Train(double hoursDelayed)
         {
-
+            _hoursDelayed = hoursDelayed;
+            _messageOnHoursDelayed = new Dictionary<double, string>();
+            _messageOnHoursDelayed.Add(0, "Nice 'n' Rolling");
+            _messageOnHoursDelayed.Add(1, "Just another day at the train");
+            _messageOnHoursDelayed.Add(1.5, "We now support doubles");
+            _messageOnHoursDelayed.Add(2, "This train is annoying me");
+            _messageOnHoursDelayed.Add(3, "B-u-u-urn it!");
         }
 
         public string GetCustomerSatisfaction()
         {
-            throw new NotImplementedException();
+            if(_messageOnHoursDelayed.ContainsKey(_hoursDelayed))
+            {
+                return _messageOnHoursDelayed[_hoursDelayed];
+            }
+
+            throw new Exception();
         }
     }
 }
---
+++ 02 - Lab 1 - SpaceBook/SpaceBook/SpaceBook/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SpaceBook
+{
+    public class Message
+    {
+        public DateTime Date { get; set; }
+        public Person Sender { get; set; }
+        public string Body { get; set; }
+
+        public Message(Person sender, string body)
+        {
+            Sender = sender;
+            Body = body;
+            Date = new DateTime();
+        }
+    }
+}
